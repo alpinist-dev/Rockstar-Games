61a07a0e2bbdec7d137eb713ac7eebfbdce2d422
alpinist <telescoper9999@gmail.com>
2025-10-17 01:34:42 +0330

    change code

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { FaShoppingCart, FaCheck } from "react-icons/fa";
-import Link from "next/link";
 
 type Product = {
   id: number;
@@ -81,9 +80,6 @@ export default function StorePage() {
           href="/cart"
           className="inline-flex items-center gap-3 bg-orange-500 text-black font-bold px-6 py-3 rounded-full hover:bg-orange-600 shadow-[0_8px_30px_rgba(255,115,0,0.2)] transition"
         >
-          <Link href="/cart">
-          <FaShoppingCart /> Go to Cart ({cart.length})
-          </Link>
         </a>
       </div>
     </div>
